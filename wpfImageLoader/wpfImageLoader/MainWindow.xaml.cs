df61d1ba1d259441a8fd110e339e753314deb23a
sunkeys1 <basatax5@gmail.com>
2023-09-09 23:01:42 +0700

    Added load image to form

---
+++ wpfImageLoader/wpfImageLoader/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfImageLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void selectIMG_Click(object sender, RoutedEventArgs e)
+        {
+            string imgName = string.Empty;
+            string imgPath = string.Empty;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.JPEG;*.JPG;*.PNG;*BMP)|*.JPEG;*.JPG;*.PNG;*BMP";
+            bool? success = ofd.ShowDialog();
+            if (success == true)
+            {
+                imgName = ofd.SafeFileName;
+                imgPath = ofd.FileName;
+                testLabel.Content = imgPath;
+                imgToDB.Source = new BitmapImage(new Uri(imgPath)); // хз почему в первый раз не сработало
+
+            }
+            
+        }
+    }
+}
