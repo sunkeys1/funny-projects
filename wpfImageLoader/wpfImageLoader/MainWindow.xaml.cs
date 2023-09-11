48b5cd8d2c13807259352d3ddc5e1bd4a70e2e82
sunkeys1 <basatax5@gmail.com>
2023-09-11 22:38:42 +0700

    Not fully pic to db adding
    not at all

--- wpfImageLoader/wpfImageLoader/MainWindow.xaml.cs
+++ wpfImageLoader/wpfImageLoader/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +28,7 @@ namespace wpfImageLoader
         {
             InitializeComponent();
         }
-
+        private string connection = "Data Source=localhost;integrated security=sspi;initial catalog=123";
         private void selectIMG_Click(object sender, RoutedEventArgs e)
         {
             string imgName = string.Empty;
@@ -43,6 +46,68 @@ namespace wpfImageLoader
 
             }
             
+        }
+
+        private void toDB_Click(object sender, RoutedEventArgs e)
+        {
+            //byte[] byte_img = File.ReadAllBytes(imgToDB.);
+            var some_img = imgToDB.Source;
+            if (imgToDB.Source != null)
+            {
+                byte[] barr = File.ReadAllBytes(testLabel.Content.ToString());
+                var img = imgToDB.Source;
+            }
+
+
+            // рабочая тема но до конца не понял
+
+
+            //SqlConnection cn = new SqlConnection(connection);
+            //SqlCommand cmd = new SqlCommand("INSERT INTO imgTest (img) VALUES (@igm)", cn);
+            //String strFilePath = testLabel.Content.ToString();
+
+            ////Read jpg into file stream, and from there into Byte array.
+            //FileStream fsBLOBFile = new FileStream(strFilePath, FileMode.Open, FileAccess.Read);
+            //Byte[] bytBLOBData = new Byte[fsBLOBFile.Length];
+            //fsBLOBFile.Read(bytBLOBData, 0, bytBLOBData.Length);
+            //fsBLOBFile.Close();
+
+            ////Create parameter for insert command and add to SqlCommand object.
+            //SqlParameter prm = new SqlParameter("@igm", SqlDbType.VarBinary, bytBLOBData.Length, ParameterDirection.Input, false,
+            //0, 0, null, DataRowVersion.Current, bytBLOBData);
+            //cmd.Parameters.Add(prm);
+
+            ////Open connection, execute query, and close connection.
+            //cn.Open();
+            //cmd.ExecuteNonQuery();
+            //cn.Close();
+
+        }
+
+        private void fromDB_Click(object sender, RoutedEventArgs e)
+        {
+            // для выгрузки из бд но пикчурбокс
+
+            //SqlConnection cn = new SqlConnection(strCn);
+            //cn.Open();
+
+            ////Retrieve BLOB from database into DataSet.
+            //SqlCommand cmd = new SqlCommand("SELECT BLOBID, BLOBData FROM BLOBTest ORDER BY BLOBID", cn);
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataSet ds = new DataSet();
+            //da.Fill(ds, "BLOBTest");
+            //int c = ds.Tables["BLOBTest"].Rows.Count;
+
+            //if (c > 0)
+            //{
+            //    //BLOB is read into Byte array, then used to construct MemoryStream,
+            //    //then passed to PictureBox.
+            //    Byte[] byteBLOBData = new Byte[0];
+            //    byteBLOBData = (Byte[])(ds.Tables["BLOBTest"].Rows[c - 1]["BLOBData"]);
+            //    MemoryStream stmBLOBData = new MemoryStream(byteBLOBData);
+            //    pictureBox1.Image = Image.FromStream(stmBLOBData);
+            //}
+            //cn.Close();
         }
     }
 }
