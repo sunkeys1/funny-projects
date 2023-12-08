73b689a2a8163e263bceba0296e04aa8715f2a71
sunkeys1 <basatax5@gmail.com>
2023-12-08 22:18:19 +0700

    Created preparing for db

---
+++ postgreWPFapp/postgreWPFapp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace postgreWPFapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void getData_Click(object sender, RoutedEventArgs e)
+        {
+            GetData();
+        }
+
+        private void clearData_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+        }
+        public void GetData()
+        {
+            string connectionString = "Server=localhost;Database=pgDB;Port=5432;User Id=postgres;Password=postg";
+            string sqlQuery = "SELECT * FROM users";
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                {
+                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    dataGrid.ItemsSource = dataTable.DefaultView;
+
+                }
+                connection.Close();
+            }
+        }
+    }
+}
