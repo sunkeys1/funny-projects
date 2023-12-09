f520f26bebe574cc28342d0a3a8ea20bd7a11259
sunkeys1 <basatax5@gmail.com>
2023-12-09 22:56:52 +0700

    Switching between select table, collaps buttons + pic

--- postgreWPFapp/postgreWPFapp/MainWindow.xaml.cs
+++ postgreWPFapp/postgreWPFapp/MainWindow.xaml.cs
@@ -25,37 +25,93 @@ namespace postgreWPFapp
         public MainWindow()
         {
             InitializeComponent();
+            
+            
         }
 
         private void getData_Click(object sender, RoutedEventArgs e)
         {
-            GetData();
+            GetData(combo.SelectedIndex);
+            var curr = combo.Items.Count;
+            //var value = (string)curr;
+            l1.Content = curr;
+
+            
         }
 
         private void clearData_Click(object sender, RoutedEventArgs e)
         {
             dataGrid.ItemsSource = null;
+            l1.Content = null;
         }
-        public void GetData()
+        public void GetData(int selected)
         {
-            string connectionString = "Server=localhost;Database=pgDB;Port=5432;User Id=postgres;Password=postg";
-            string sqlQuery = "SELECT * FROM users";
-
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            string table = "";
+            string[] sqlQrrys = { "users", "games" };
+            switch (selected)
             {
-                connection.Open();
+                case 0:
+                    table = "users";
+                    break;
+                case 1:
+                    table = "games";
+                    break;
 
-                using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
-                {
-                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(command);
-                    DataTable dataTable = new DataTable();
-                    dataAdapter.Fill(dataTable);
 
-                    dataGrid.ItemsSource = dataTable.DefaultView;
-
-                }
-                connection.Close();
+                default:
+                    break;
             }
+            if (sqlQrrys.Contains(table))
+            {
+
+                string connectionString = "Server=localhost;Database=pgDB;Port=5432;User Id=postgres;Password=postg";
+                string sqlQuery = $"SELECT * FROM {table}";
+
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    {
+                        NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        dataAdapter.Fill(dataTable);
+
+                        dataGrid.ItemsSource = dataTable.DefaultView;
+
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        private void change_Click(object sender, RoutedEventArgs e)
+        {
+            var collaps = Visibility.Collapsed;
+            var visible = Visibility.Visible;
+            if (clearData.Visibility == visible)
+            {
+                clearData.Visibility = collaps;
+                dataGrid.Visibility = collaps;
+                combo.Visibility = collaps;
+                getData.Visibility = collaps;
+                l1.Visibility = collaps;
+                img.Visibility = visible;
+                change.Content = "To Data";
+
+            }
+            else
+            {
+
+                clearData.Visibility = visible;
+                dataGrid.Visibility = visible;
+                combo.Visibility = visible;
+                getData.Visibility = visible;
+                l1.Visibility = visible;
+                img.Visibility = collaps;
+                change.Content = "To Settings";
+            }
+            
         }
     }
 }
